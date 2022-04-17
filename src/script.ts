ac63a8894950b368fab4c9ce3c98e6a1040acfa0
Michael Silva <michaelsilv@gmail.com>
2022-04-17 02:16:02 -0300

    Criação do objeto que armazena os algarismo romanos. Configuração da folha de estilo e edição do index.

---
+++ src/script.ts
@@ -0,0 +1,30 @@
+let numerosRomanos = [
+   {romano:'M', numero:1000},
+   {romano:'CM', numero:900},
+   {romano:'D', numero:500},
+   {romano:'CD', numero:400},
+   {romano:'C', numero:100},
+   {romano:'XC', numero:90},
+   {romano:'L', numero:50},
+   {romano:'XL', numero:40},
+   {romano:'X', numero:10},
+   {romano:'IX', numero:9},
+   {romano:'V', numero:5},
+   {romano:'IV', numero:4},
+   {romano:'I', numero:1},
+];
+const I: number = 1;
+const V: number = 5;
+const X: number = 10;
+const L: number = 50;
+const C: number = 100;
+const D: number = 500;
+const M: number = 1000;
+let btn = document.getElementById('botao');
+
+
+
+
+function converter (){
+   let resultado = document.getElementById('algarismo');
+}
